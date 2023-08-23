687dc9b6a84432bc34d9fff295e8017a1b26d3b0
sachaeggel <sacha.eggel@gmail.com>
2023-08-23 11:16:20 -0300

    ft: el 4 se modifico, agregar al 3 y terminar de imprimir bien

--- i_4.py
+++ i_4.py
@@ -1,9 +1,14 @@
-diccionario = {1:"sacha", 5:"augusto", 3:"eggel", 5:"sanguche", 1:"milanesa"}
+diccionario = {"sacha":1, "augusto":5, "eggel":3, "sanguche":5, "milanesa":1}
 
 def tupla(a):
-    x = a.split()
+    x = list(a.values())
+    mayor = max(x)
+    final=[]
+    for palabra in a:
+        valor = a[palabra]
+        if valor >= mayor:
+           x = final.append(palabra)
     return x
 
 sacha = tupla(diccionario)
-
 print(sacha)
