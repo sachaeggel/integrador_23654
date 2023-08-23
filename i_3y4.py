fade3e9c3b314c06fa988586aaf5ee7729f84b99
informatica <sacha.eggel@gmail.com>
2023-08-23 15:26:23 -0300

    ft: terminé el 4

--- i_3y4.py
+++ i_3y4.py
@@ -10,5 +10,18 @@ def separador(chain):
         diccionario[palabra]=frecuencia
     return diccionario
 
+def tupla(a):
+    x = list(a.values())
+    mayor = max(x)
+    final=[]
+    for palabra in a:
+        valor = a[palabra]
+        if valor >= mayor:
+            conc=palabra, valor
+            final.extend(conc)
+    return tuple(final)
+
 dic = separador(oracion)
 print(f"Diccionario PALABRA : FRECUENCIA\n{dic}")
+a = tupla(dic)
+print(f"PALABRA más repetida y FRECUENCIA:{a}")
